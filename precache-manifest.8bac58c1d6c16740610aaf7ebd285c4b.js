8537a1e9ba05de5282f44f873d05e141764d4d0e
Angel Wu <willjxc@gmail.com>
2019-01-23 11:56:29 -0500

    Updates

---
+++ precache-manifest.8bac58c1d6c16740610aaf7ebd285c4b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2a5e834022b97fa8bc3d",
+    "url": "/Pics-search--React/static/css/main.13f61f6d.chunk.css"
+  },
+  {
+    "revision": "2a5e834022b97fa8bc3d",
+    "url": "/Pics-search--React/static/js/main.2a5e8340.chunk.js"
+  },
+  {
+    "revision": "102c3bf24ee7bc42981e",
+    "url": "/Pics-search--React/static/js/1.102c3bf2.chunk.js"
+  },
+  {
+    "revision": "b306d63fc0ee49f3ed92",
+    "url": "/Pics-search--React/static/js/runtime~main.b306d63f.js"
+  },
+  {
+    "revision": "8887a9c78f6d8b89ed5b4a4efd9e13b5",
+    "url": "/Pics-search--React/index.html"
+  }
+];
